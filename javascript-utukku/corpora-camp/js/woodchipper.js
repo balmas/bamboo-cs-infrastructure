0c21fe6153df275724a60df84079be8e0f45f64f
William Garr <wjg8@georgetown.edu>
2011-03-03 19:59:34 -0500

    jQuery version of drawing, interaction code for displays -- still todo: drag selection.

--- javascript-utukku/corpora-camp/js/woodchipper.js
+++ javascript-utukku/corpora-camp/js/woodchipper.js
@@ -1,105 +1,155 @@
-$(window).load(function () {
-	// Creates canvas 320 × 200 at 10, 50
-	window.canvas = Raphael("canvas", 1000, 600);
-	
-  canvas.jQ = $("#canvas");
-  
-  $.getJSON('../html/data.json', function(data){ 
-    canvas.data = data;
-    canvas.draw();
-  });
+/*
+ * jQuery initialization. Allow for multiple display ports on a page.
+ * Each could have its own WebSocket connection. They could be spawned by queries, for visual comparisons.
+ */
+$(function() {
+  window.data_displays = $(".display_port").each(function(){
+    $.extend(this, {
+      // create a Raphael object based on the element
+      jQ: null,
+      data: null,
+      drawer:null,
+      width: 0,
+      height: 0,
+      
+        init: function(){
+          // set up display drawing.
+          this.jQ = $(this);
+          this.width = this.jQ.width();
+          this.height = this.jQ.height();
+          this.drawer = Raphael(this, this.width, this.height);
+          // set up interaction.
+          this.jQ.mousedown(this.mousedown)
+          // get initial data.
+          this.get_data();
+        },
+      
+        get_data: function(){
+          var display_port = this;
+          $.getJSON('../html/data.json', function(data){ 
+            display_port.data = data;
+            display_port.draw();
+          });
+        },
+      
+        draw: function(){
+          var display_port = this;
+          $.each(this.data, function(key, val) {
+              $.each(val, function(a, b) {
+                  $.each(b, function(c, d) {      
+                    x = d['x'] + 1;
+                    y = d['y'] + 1;
+                    
+                    x_multiplier = display_port.width / 2.0;
+                    y_multiplier = display_port.height / 2.0;
 
-  canvas.draw = function(){
-      $.each(this.data, function(key, val) {
-          $.each(val, function(a, b) {
-              $.each(b, function(c, d) {
+                    var circle = display_port.drawer.circle(x*x_multiplier, y*y_multiplier, 2);
+                    circle.attr("stroke", d['color']);
 
-                  x = d['x'] + 1;
-                  y = d['y'] + 1;
-
-                  var circle = canvas.circle(x*500, y*300, 2);
-                  circle.attr("stroke", d['color']);
-				
-				circle.node.id= d['id'];
-				circle.node.onclick=function(){alert(this.id)};
+                    circle.node.id= d['id'];
+                    circle.node.onclick=function(){alert(this.id)};
+                  });
               });
           });
-      });
-  }
-  
-  
-  canvas.zoom = function(){
-    this.draw();
-  }
-  
-  // Create select box
-	canvas.jQ.selectBox = $('<div/>')
-						.appendTo(canvas.jQ)
-						.attr('class', 'selector')
-						.css('position', 'absolute');
-  
-  canvas.jQ.mousedown(function (e) {
-		canvas.jQ.showSelectBox(e);
-	});
-	
-	canvas.jQ.mousemove(function (e) {
-		canvas.jQ.refreshSelectBox(e);
-	});
-	
-	// Shows the select box
-	canvas.jQ.showSelectBox = function (e) {
-		if (parent.is('.' + config.disabledClass)) {
-			return;
-		}
-    selectBoxOrigin = {}
-		selectBoxOrigin.left	= e.pageX - parentDim.left + parent[0].scrollLeft;
-		selectBoxOrigin.top		= e.pageY - parentDim.top + parent[0].scrollTop;
-
-		var css = {
-			left:		selectBoxOrigin.left + 'px', 
-			top:		selectBoxOrigin.top + 'px', 
-			width:		'1px', 
-			height:		'1px'
-		};
-		selectBox.addClass(config.activeClass).css(css);
-	};
-
-	// Refreshes the select box dimensions and possibly position
-	canvas.jQ.refreshSelectBox = function (e) {
-
-		var left		= e.pageX - parentDim.left + parent[0].scrollLeft;
-		var top			= e.pageY - parentDim.top + parent[0].scrollTop;
-		var newLeft		= left;
-		var newTop		= top;
-		var newWidth	= selectBoxOrigin.left - newLeft;
-		var newHeight	= selectBoxOrigin.top - newTop;
-
-		if (left > selectBoxOrigin.left) {
-			newLeft		= selectBoxOrigin.left;
-			newWidth	= left - selectBoxOrigin.left;
-		}
-
-		if (top > selectBoxOrigin.top) {
-			newTop		= selectBoxOrigin.top;
-			newHeight	= top - selectBoxOrigin.top;
-		}
-
-		var css = {
-			left:	newLeft + 'px', 
-			top:	newTop + 'px', 
-			width:	newWidth + 'px', 
-			height:	newHeight + 'px'
-		};
-		selectBox.css(css);
-	};
-	
-	canvas.jQ.mouseup(function (e) {
-		hideSelectBox(e);
-    // pass info about our selection rect to the canvas & have it redraw itself.
-		canvas.zoom();
-	});
-	
-	
-  
-
+        },
+        
+        mousedown: function(event){
+          alert(unpack(event))
+        }
+    });
+    
+    this.init();
+  });
 });
+
+
+// Utils.
+
+function unpack(obj, delimiter){
+  if (obj == undefined){ return "undefined" };
+	var report = new Array();
+	for (var m in obj){
+		var member = obj[m];
+		if (!(member instanceof Function)){
+			if (member != undefined){ report.push(m +":"+ member); }
+		}
+	}
+	delimiter = delimiter || "\t";
+	return report.join(delimiter);
+}
+
+
+
+// 
+// 
+// canvas.zoom = function(){
+//   this.draw();
+// }
+
+// // Create select box
+// canvas.jQ.selectBox = $('<div/>')
+//          .appendTo(canvas.jQ)
+//          .attr('class', 'selector')
+//          .css('position', 'absolute');
+// 
+// canvas.jQ.mousedown(function (e) {
+//  canvas.jQ.showSelectBox(e);
+// });
+// 
+// canvas.jQ.mousemove(function (e) {
+//  canvas.jQ.refreshSelectBox(e);
+// });
+// 
+// // Shows the select box
+// canvas.jQ.showSelectBox = function (e) {
+//  if (parent.is('.' + config.disabledClass)) {
+//    return;
+//  }
+//   selectBoxOrigin = {}
+//  selectBoxOrigin.left  = e.pageX - parentDim.left + parent[0].scrollLeft;
+//  selectBoxOrigin.top   = e.pageY - parentDim.top + parent[0].scrollTop;
+// 
+//  var css = {
+//    left:   selectBoxOrigin.left + 'px', 
+//    top:    selectBoxOrigin.top + 'px', 
+//    width:    '1px', 
+//    height:   '1px'
+//  };
+//  selectBox.addClass(config.activeClass).css(css);
+// };
+// 
+// // Refreshes the select box dimensions and possibly position
+// canvas.jQ.refreshSelectBox = function (e) {
+// 
+//  var left    = e.pageX - parentDim.left + parent[0].scrollLeft;
+//  var top     = e.pageY - parentDim.top + parent[0].scrollTop;
+//  var newLeft   = left;
+//  var newTop    = top;
+//  var newWidth  = selectBoxOrigin.left - newLeft;
+//  var newHeight = selectBoxOrigin.top - newTop;
+// 
+//  if (left > selectBoxOrigin.left) {
+//    newLeft   = selectBoxOrigin.left;
+//    newWidth  = left - selectBoxOrigin.left;
+//  }
+// 
+//  if (top > selectBoxOrigin.top) {
+//    newTop    = selectBoxOrigin.top;
+//    newHeight = top - selectBoxOrigin.top;
+//  }
+// 
+//  var css = {
+//    left: newLeft + 'px', 
+//    top:  newTop + 'px', 
+//    width:  newWidth + 'px', 
+//    height: newHeight + 'px'
+//  };
+//  selectBox.css(css);
+// };
+// 
+// canvas.jQ.mouseup(function (e) {
+//  hideSelectBox(e);
+//   // pass info about our selection rect to the canvas & have it redraw itself.
+//  canvas.zoom();
+// });
+// 
